20af074149e9ee29b6c1f59cb797eb562a11e2c1
diane2007 <wangruochong0406@gmail.com>
2023-04-14 12:47:43 -0400

    OnMouseOver decided to work today.

--- W10EggMatch3/Assets/Scripts/SwapPlaceScript.cs
+++ W10EggMatch3/Assets/Scripts/SwapPlaceScript.cs
@@ -10,39 +10,29 @@ public class SwapPlaceScript : MonoBehaviour
     bool enterCol;
     bool isClicked;       //whether we have clicked on obj1 or not
 
-    void OnMouseEnter()
+    void OnMouseOver()
     {
-        Debug.Log("Entered!");
-        enterCol = true;
-    }
+        //Debug.Log("Over!");
+        //enterCol = true;
 
-    void OnMouseExit()
-    {
-        enterCol = false;
-    }
-
-    void Update()
-    {
-        if(enterCol)
+        if (Input.GetMouseButtonDown(0))
         {
-            if (Input.GetMouseButtonDown(0))
+            Debug.Log("Clicked!");
+
+            if (GameManager.instance.object1 == null)
             {
-                Debug.Log("Clicked!");
-                
-                if (GameManager.instance.object1 == null)
-                {
-                    Debug.Log("Object 1 clicked!");
-                    GameManager.instance.object1 = transform;
-                }
-                //object 2 is empty and is not already selected as object 1
-                else if(!GameManager.instance.object2  && GameManager.instance.object1 != transform)
-                {
-                    Debug.Log("Object 2 clicked!");
-                    GameManager.instance.object2 = transform;
-                }
+                Debug.Log("Object 1 clicked!");
+                GameManager.instance.object1 = transform;
+            }
+            
+            //object 2 is empty and is not already selected as object 1
+            else if(!GameManager.instance.object2  && GameManager.instance.object1 != transform)
+            {
+                Debug.Log("Object 2 clicked!");
+                GameManager.instance.object2 = transform;
             }
         }
     }
 
-    
+
 }
